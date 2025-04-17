21afdb1d49d175ffbf8c5e27ac65e5a7adc4c3df
lakhanlodhi21 <sikandarlodhi693@gmail.com>
2025-04-17 21:35:36 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,24 @@
+let store = ''
+let tap = (value) => {
+    let cal = document.querySelector("#display")
+    store = store + value
+    cal.innerHTML = store
+}
+
+let result = () => {
+    let cal = document.querySelector("#display")
+    store = eval(store)
+    cal.innerHTML= store
+}
+    
+let empty = () => {
+    let cal = document.querySelector("#display")
+    store = eval()
+    cal.innerHTML= ""
+}
+
+let del = () => {
+    let cal = document.querySelector("#display")
+    store=store.slice(0, -1)
+    cal.innerHTML= store
+}
